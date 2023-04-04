63d5881426ae98bc67e90b2c35e9cb6ba3559f66
Sushant Nyachhyon <sushantnyachhyon@gmail.com>
2023-04-04 18:19:03 +1000

    feat: add config file for application constants

---
+++ src/config/index.ts
@@ -0,0 +1,6 @@
+export const appName = import.meta.env.VITE_APP_NAME as string;
+export const baseURL = import.meta.env.VITE_UNSPLASH_BASE_URL as string;
+export const unsplashAccessKey =
+    import.meta.env.VITE_UNSPLASH_ACCESS_KEY as string;
+export const unsplashSecretKey =
+    import.meta.env.VITE_UNSPLASH_SECRET_KEY as string;
