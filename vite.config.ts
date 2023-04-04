65b137a6789631c396c3f21bc38ad54c33ce1cf1
Sushant Nyachhyon <sushantnyachhyon@gmail.com>
2023-04-04 18:17:04 +1000

    chore: configure vitest with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(path.resolve(), './src')
+        }
+    },
+    test: {
+        globals: true,
+        cache: {
+            dir: './node_modules/.vitest'
+        },
+        environment: 'jsdom',
+        include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
+    }
+});
