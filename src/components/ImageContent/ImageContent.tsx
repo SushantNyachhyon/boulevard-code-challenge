a4b5de62d4c5b73fdae6788982e7e9f0bdb68975
Sushant Nyachhyon <sushantnyachhyon@gmail.com>
2023-04-05 16:01:34 +1000

    feat: import modal component
    - use modal component as lightbox to show large image

--- src/components/ImageContent/ImageContent.tsx
+++ src/components/ImageContent/ImageContent.tsx
@@ -1,18 +1,41 @@
 import type { FC } from 'react';
+import { useState } from 'react';
 
 import Image from '@/components/Image/Image';
+import Modal from '@/components/Modal/Modal';
 import { IUnsplash } from '@/types/unsplash.type';
 
-const ImageContent: FC<{ data: IUnsplash }> = (props) => (
-    <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-10 mt-10'>
-        {
-            props.data.results.map(image => (
-                <Image 
-                    key={image.id} 
-                    data={image} />
-            ))
-        }
-    </div>
-);
+const ImageContent: FC<{ data: IUnsplash }> = (props) => {
+    const [show, setShow] = useState(false);
+    const [imgSrc, setImgSrc] = useState('');
+
+    return (
+        <>
+            <Modal
+                show={show}
+                onClose={() => { setShow(false); }}>
+                <img src={imgSrc} className='f-full h-full object-contain' />
+            </Modal>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-10 mt-10'>
+                {
+                    props.data.results.map(image => (
+                        <a
+                            href="#"
+                            key={image.id}
+                            onClick={(e) => {
+                                e.preventDefault();
+                                setShow(true);
+                                setImgSrc(image.urls.raw);
+                            }}>
+                            <Image
+                                data={image} />
+
+                        </a>
+                    ))
+                }
+            </div>
+        </>
+    );
+};
 
 export default ImageContent;
