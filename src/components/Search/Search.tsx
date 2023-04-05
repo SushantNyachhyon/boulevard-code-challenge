82f4503035e9d471611327ba2073bd06d1197bbd
Sushant Nyachhyon <sushantnyachhyon@gmail.com>
2023-04-05 13:42:42 +1000

    feat: add SearchDrownDown Component and configure key events
    - import SearchDropDown Component and display
    - add events to handle focus and blur on input field to display the dropdown
    - add key events to handle focus and blur

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,8 +1,10 @@
-import type { FC } from 'react';
-import { useState } from 'react';
+import { FC } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useSearchParams, useNavigate } from 'react-router-dom';
 import { MagnifyingGlass } from '@phosphor-icons/react';
 
+import { Recent } from '@/services/recent.service';
+import SearchDropDown from './SearchDropDown';
 import { Input } from '@/components/Input';
 import Styles from './search.module.css';
 
@@ -15,29 +17,75 @@ type SearchProps = {
 const Search: FC<SearchProps> = (props) => {
     const navigate = useNavigate();
     const [searchParams] = useSearchParams();
-    const [query, setQuery] = useState<string>(searchParams.get('q') || '');
+    const [query, setQuery] = useState(searchParams.get('q') || '');
+    const [isVisible, setIsVisible] = useState(false);
+    const inputEl = useRef<HTMLInputElement>(null);
+    const searchContainer = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (
+                inputEl.current &&
+                !inputEl.current.contains(event.target as Node) &&
+                searchContainer.current &&
+                !searchContainer.current.contains(event.target as Node)
+            ) {
+                setIsVisible(false);
+            }
+        };
+
+        const handleSearchKey = (e: KeyboardEvent) => {
+            if (e.ctrlKey && e.key === '/') {
+                inputEl.current &&
+                    inputEl.current.focus();
+            }
+            if (e.key === 'Escape') {
+                inputEl.current &&
+                    inputEl.current.blur();
+                setIsVisible(false);
+            }
+        };
+
+        window.addEventListener('keyup', handleSearchKey);
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+            window.removeEventListener('keyup', handleSearchKey);
+        };
+    }, [inputEl]);
 
     return (
         <div
             className={`w-full relative ${props.classList}`}>
             <Input
                 classList={props.inputClass}
-                placeholder={'Search for Images ...'}
+                placeholder={'Search for Images ... ' +
+                    'Press (Ctrl + /) to focus'}
                 value={query}
                 onChange={e => {
                     setQuery(
                         (e.target as HTMLInputElement).value);
                 }}
-                onKeyUp={e => {
+                onKeyUp={async e => {
                     if (e.key === 'Enter') {
+                        await Recent.storeRecentSearch(query);
+                        setIsVisible(false);
                         navigate(`/search?q=${query}`, {
                             replace: true
                         });
                     }
-                }} />
+                }}
+                onFocus={() => {
+                    setIsVisible(true);
+                }}
+                ref={inputEl} />
             <MagnifyingGlass
                 className={Styles.searchIcon}
                 size={props.iconSize} />
+            <SearchDropDown
+                visible={isVisible}
+                ref={searchContainer} />
         </div>
     );
 };
