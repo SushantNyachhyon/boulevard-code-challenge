646dbd55338315755e2ee22212dbd2ad7532a515
Sushant Nyachhyon <sushantnyachhyon@gmail.com>
2023-04-05 18:04:40 +1000

    feat: implement top pic
    - fetch top picks from unsplash
    - create infinite load more button to fetch top pick

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,54 +1,81 @@
 import type { FC } from 'react';
 
+import { useUnsplashTopQuery } from '@/queries/unsplash.query';
 import { Search } from '@/components/Search';
 import { ImageContent } from '@/components/ImageContent';
 import { ImageSkeleton } from '@/components/ImageSkeleton';
+import Spinner from '@/components/Spinner/Spinner';
 import Styles from './home.module.css';
-import { data } from '@/data';
 
-const Home: FC = () => (
-    <main>
-        <section
-            className={Styles.landingSection}>
-            <div
-                className="app-container">
+const Home: FC = () => {
+    const {
+        data,
+        isLoading,
+        isFetchingNextPage,
+        fetchNextPage,
+        hasNextPage
+    } = useUnsplashTopQuery(1);
+
+    return (
+        <main>
+            <section
+                className={Styles.landingSection}>
                 <div
-                    className={Styles.landingContainer}>
-                    <h1
-                        className={Styles.landingHeading}>
-                        Boulevard
-                    </h1>
-                    <span
-                        className={Styles.landingSubHeading}>
-                        Search Your Imagination
-                    </span>
-                    <Search
-                        classList='mt-10'
-                        inputClass={'!h-14'}
-                        iconSize={26} />
-                    <span
-                        className={Styles.helperText}>
-                        Powered by {' '}
-                        <a
-                            href="https://unsplash.com"
-                            target="_blank">
-                            Unsplash
-                        </a>
-                    </span>
+                    className="app-container">
+                    <div
+                        className={Styles.landingContainer}>
+                        <h1
+                            className={Styles.landingHeading}>
+                            Boulevard
+                        </h1>
+                        <span
+                            className={Styles.landingSubHeading}>
+                            Search Your Imagination
+                        </span>
+                        <Search
+                            classList='mt-10'
+                            inputClass={'!h-14'}
+                            iconSize={26} />
+                        <span
+                            className={Styles.helperText}>
+                            Powered by {' '}
+                            <a
+                                href="https://unsplash.com"
+                                target="_blank">
+                                Unsplash
+                            </a>
+                        </span>
+                    </div>
                 </div>
-            </div>
-        </section>
-        <section className='py-20'>
-            <div className="app-container">
-                <h2
-                    className="text-2xl text-gray-600 font-semibold">
-                    Browse Our Top Picks
-                </h2>
-                <ImageSkeleton />
-                <ImageContent data={data} />
-            </div>
-        </section>
-    </main>
-);
+            </section>
+            <section className='py-20'>
+                <div className="app-container">
+                    <h2
+                        className="text-2xl text-gray-600 font-semibold">
+                        Browse Our Top Picks
+                    </h2>
+                    {
+                        isLoading
+                            ? <ImageSkeleton />
+                            : data?.pages.map((item, index) => (
+                                <ImageContent data={item} key={index} />
+                            ))
+                    }
+                    <div
+                        className="flex flex-col w-full items-center mt-5 ">
+                        {isFetchingNextPage ? <Spinner size={50} /> : <></>}
+                        <button
+                            className={'text-sm text-gray-700 border mt-5'
+                                + ' border-gray-400 rounded px-8 py-3'}
+                            onClick={() => { fetchNextPage(); }}
+                            disabled={!hasNextPage}>
+                            Load More
+                        </button>
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
+};
 
 export default Home;
